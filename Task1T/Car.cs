d97de10860cc0e2d888f5bf997e157f188a3f375
Vlad <vlad.kuzmichw@gmail.com>
2018-10-25 01:13:27 +0300

    first

--- Task1T/Car.cs
+++ Task1T/Car.cs
@@ -13,5 +13,11 @@ namespace Task1T
         private double _height;
         private double _weight;
         private string _color;
+
+        public double Length { get => _length; set => _length = value; }
+        public double Width { get => _width; set => _width = value; }
+        public double Height { get => _height; set => _height = value; }
+        public double Weight { get => _weight; set => _weight = value; }
+        public string Color { get => _color; set => _color = value; }
     }
 }
